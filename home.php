bf7b2645ab36fba44fe0b5627358555f97bf1a47
shadoweminence <105636972+shadoweminence@users.noreply.github.com>
2023-05-02 12:51:05 +0545

    Add files via upload

--- home.php
+++ home.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,44 +7,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gharwala</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="design.css">
+    <link rel="stylesheet" href="style.css"/>
+    <script src="abc.js"></script>
+    
 </head>
 <body>
  
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a href="home.php">
-    <img src="Gharwala.jpg" alt="logo"  style="height: 100px;width: 200px;">
-  </a>
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="overview.php">Overview</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="login.php">Log In</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page"  href="register.html">Register</a>
-        </li>
-     
-          </ul>
-          
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-    </div>
-  </div>
-</nav><br><br><br>
+    
 <div class="conatainer">
-  <div class="left-column" >
-        <p style="width: 40%;">
+  <center><h1>Gharwala</h1>
+  <img src='img/logo.jpeg' alt="logo of gharwala"/>
+  
+  </center>
+        <p >
         Introducing <b>"Gharwala"</b>, the ultimate solution for all your household problems! Whether you're dealing with a leaky faucet, a blown fuse, or simply need a fresh coat of paint, Gharwala has got you covered.
 
       With a team of skilled professionals from a range of trades, including plumbing, electricity, painting, and more, you can trust that your home is in good hands. Our experts are not only highly trained and experienced, but also friendly, reliable, and committed to delivering exceptional results every time.
@@ -51,26 +28,41 @@
 
       So why wait? Join the <b>Gharwala</b> community today and say goodbye to household problems once and for all!
       </p>
-
-      <div>
-        <img src="Gharwala.png"/>
-      </div>
-
-
-</div>
-<div class="right-column">
-  <b><h1>Reviews</h1></b>
-  <p>   <h1>Rajesh Koirala</h1> "I had a plumbing emergency in the middle of the night and HomePro was there to save the day! The plumber arrived within 30 minutes and was able to fix the issue quickly and efficiently. I was so impressed by their prompt response and quality service."</p>
-
-  <p>"I recently hired HomePro to paint my entire house and I couldn't be happier with the results. The team was professional, courteous, and took great care to ensure that the job was done right. They even helped me pick out the perfect colors for each room! I would highly recommend HomePro to anyone looking for a reliable and affordable painting service."
-  </p>  
-   <p> "I had been having issues with my electrical system for months and couldn't figure out what was wrong. That is, until I called HomePro. Their electrician was able to identify the problem right away and fix it in no time. I was so relieved and grateful for their expertise."
-  </p>
-   <p> "I was hesitant to hire a handyman for fear of getting shoddy work, but HomePro completely exceeded my expectations. Their handyman was knowledgeable, skilled, and took the time to explain everything he was doing. He even went above and beyond to fix a few other small issues around my home. I will definitely be using HomePro again!"</p>
-</div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<center><h1>Our services</h1></center><br><br>
+
+<h2>Plumbing</h2>
+
+<div class="image-container">
+  <ul>
+    <li><img src='img/p1.png' alt="plumbing"/></li>
+    <li><img src='img/plumbing.png' alt="plumbing"/></li>
+    <li><img src='img/p2.png' alt="plumbing"/></li>
+    <li><img src='img/p3.png' alt="plumbing"/></li>
+  </ul>
+</div><br><br>
+
+
+<h2>Electricians</h2>
+<div class="image-container">
+  <ul>
+    <li><img src='img/e1.png' alt="electrician"/></li>
+    <li><img src='img/e2.png' alt="electrician"/></li>
+    <li><img src='img/e3.png' alt="electrician"/></li>
+  </ul>
+</div>
+<br><br>
+
+<h2>Painters</h2>
+<div class="image-container">
+  <ul>
+    <li><img src='img/pa1.png' alt="painting"/></li>
+    <li><img src='img/pa3.png' alt="painting"/></li>
+    <li><img src='img/pa4.png' alt="painting"/></li>
+    <li><img src='img/pa5.png' alt="painting"/></li>
+  </ul>
+</div>
 
 </body>
 </html>
